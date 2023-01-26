76cec1b5020086fb68a72f0b866448b69d9ba9f0
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-01-26 09:25:51 +0600

    Add files via upload

---
+++ Basic/Condition/codition.py
@@ -0,0 +1,10 @@
+marks = int(input("Enter of Marks:"))
+
+if marks > 100 and marks < 0:
+    print("Sorry invallied marks")
+
+elif marks > 50 and marks <= 100:
+    print("A+")
+else:
+    print("Fail")
+#Must looking the space and extra line
