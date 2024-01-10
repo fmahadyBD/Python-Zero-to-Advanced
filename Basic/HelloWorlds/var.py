c0fd7375b95b030c3edbf78fdb91969a7bbeded4
Mahady Hasan Fahim <fmahady01@gmail.com>
2024-01-10 21:12:07 +0100

    update

---
+++ Basic/HelloWorlds/var.py
@@ -0,0 +1,29 @@
+# x=5
+# y=9
+# print(x+y)
+
+# print(x)
+# print(y)
+# z=float(x)
+# zz=str(x)
+# print(z)
+# print(zz)
+# print(type(x))
+# print(type(zz))
+
+
+# Mutipale value
+# x,y,z="Mahady ","Hasan","Fahim"
+# print(x)
+# print(y)
+# print(z)
+
+# x=y=z="Fahiim"
+# print(x)
+
+#Unpack acollection
+# nams=["Mahady","Hasan","Fahim"]
+# x,y,z=nams
+# print(x)
+# print(y)
+# print(z)
